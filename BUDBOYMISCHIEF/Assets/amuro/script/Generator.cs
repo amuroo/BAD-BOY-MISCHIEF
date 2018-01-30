b8a115f67424b6dd096bc4a84859c65d47847636
c17001 amuro <c17001@std.it-college.ac.jp>
2018-01-30 17:09:27 +0900

    ジェネレータのプロトタイプの作成

---
+++ BUDBOYMISCHIEF/Assets/amuro/script/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject[] enemy;
+    public float time;
+
+    //湧きポジの指定
+    public Vector2[] Gpos = { new Vector2(-405, 1000), new Vector2(-135, 1000),
+                              new Vector2(135, 1000), new Vector2(405, 1000) };
+    
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Generate");
+    }
+
+    IEnumerator Generate()
+    {
+        while (true)
+        {
+            //配列の中からランダムで生成
+            GameObject element = enemy[Random.Range(0, enemy.Length)];
+            Instantiate(element, Gpos[Random.Range(0, Gpos.Length)], Quaternion.identity);
+            yield return new WaitForSecondsRealtime(time);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
