add947d41e303f27126a0ae7022fa592e127bfec
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-02-01 14:55:25 +0900

    prot用に追加

--- BUDBOYMISCHIEF/Assets/katuki/Script/sensei.cs
+++ BUDBOYMISCHIEF/Assets/katuki/Script/sensei.cs
@@ -2,47 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sensei : MonoBehaviour {
-    private Rigidbody2D rb2d;
+public class sensei : MonoBehaviour
+{
     public float x = 1;
     private float time;
+    //private Rigidbody2D rb2d;
     private bool stan = false;
-    private float R;
     private float xx;
-	// Use this for initialization
-	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
+    private bool baku = false;
+    private Animator anim;
+    private float X = 0;
+    private float senseiX;
+    private float kaneX;
+    // Use this for initialization
+    void Start()
+    {
         xx = x;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate ( new Vector2(0, xx * -10));
-        if (stan)
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(X);
+        transform.Translate(new Vector2(X, xx * -10));
+        if (stan || baku)
         {
             time += Time.deltaTime;
-            if(time >= 3)
+            if (stan && time >= 3)
             {
                 stan = false;
                 xx = x;
+                anim.SetBool("break", false);
+            }
+            else if (baku && time >= 1)
+            {
+                Destroy(gameObject);
             }
         }
-        time += Time.deltaTime;
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Bakuchiku")
+        print("A"); if (other.gameObject.tag == "Bakuchiku")
         {
-            Destroy(gameObject);
-        }else if(other.gameObject.tag == "Gamu")
+            xx = 0;
+            anim.SetBool("break", true);
+            baku = true;
+            //Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "Gamu")
         {
             time = 0;
             xx = 0;
             stan = true;
-        }else if(other.gameObject.tag == "Dog")
+            anim.SetBool("break", true);
+        }
+        else if (other.gameObject.tag == "Dog")
         {
+            anim.SetBool("nige", true);
             xx = -1.5f;
         }
+        /*else if (other.gameObject.tag == "kane")
+        {
+            kaneX = other.gameObject.transform.position.x;
+            senseiX = transform.position.x;
+            Debug.Log(kaneX);
+            if (kaneX > senseiX + 1)
+            {
+                while (kaneX != senseiX)
+                {
+                    X = 5;
+                }
+                X = 0;
+            }
+            else if (kaneX < senseiX - 1)
+            {
+                while (kaneX != senseiX)
+                {
+                    X = -5;
+                }
+                X = 0;
+            }
+        }*/
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        senseiX = transform.position.x;
+        
     }
 }
+
+
