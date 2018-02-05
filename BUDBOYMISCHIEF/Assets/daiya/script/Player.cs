464480ba6f75fce7799d2b3bb29105f0c071a393
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2018-02-05 14:22:02 +0900

    画像の追加

--- BUDBOYMISCHIEF/Assets/daiya/script/Player.cs
+++ BUDBOYMISCHIEF/Assets/daiya/script/Player.cs
@@ -8,44 +8,59 @@ public class Player : MonoBehaviour {
     public GameObject bakutiku;
     bool gamuflag;
     bool bakutikuflag;
- 
 
+    public float gamutime = 2.0f;
+    public float bakutikutime = 2.0f;
 
-
-
+        
 
     // Use this for initialization
     void Start () {
         gamuflag = false;
         bakutikuflag = false;
-      
+
 
     }
 
     // Update is called once per frame
     void Update() {
 
+        ItemTimer();
+        ScreenTouch();
+
+      
+    }
+
+    void ScreenTouch() {
         if (Input.touchCount > 0) {
             if (Input.GetTouch(0).phase == TouchPhase.Began) {
                 Vector3 ScreenPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
                 ScreenPoint.z = 1.0f;
-                print(ScreenPoint);
+                //print(ScreenPoint);
 
-                //Vector3 nowPos = Camera.main.ScreenToWorldPoint(ScreenPoint);
                 if (ScreenPoint.y >= -600) {
-                    if (gamuflag) {
+                    if (gamuflag && gamutime <= 0) {
                         Instantiate(gamu, ScreenPoint, Quaternion.identity);
+
+                        gamutime = 2.0f;
                     }
+
                     else if (bakutikuflag) {
                         Instantiate(bakutiku, ScreenPoint, Quaternion.identity);
+
                     }
                 }
             }
         }
     }
 
+
+    void ItemTimer() {
+        gamutime -= Time.deltaTime;
+        bakutikutime -= Time.deltaTime;
+    }
+
     public void Gamu() {
-        print("a");
         gamuflag = true;
         bakutikuflag = false;
     }
@@ -54,5 +69,4 @@ public class Player : MonoBehaviour {
         bakutikuflag = true;
         gamuflag = false;
     }
-
 }
