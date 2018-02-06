7d6f1558b2fbdbdd06cc83c1a6c0caabeea8db27
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-02-06 11:35:44 +0900

    Mainシーンにタイムを追加

---
+++ BUDBOYMISCHIEF/Assets/katuki/Script/senseiDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class senseiDestroy : MonoBehaviour {
+    public bool count = false;
+    private float time;
+    public float death;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (count)
+        {
+            time += Time.deltaTime;
+            if(time >= death)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        count = true;   
+    }
+}
