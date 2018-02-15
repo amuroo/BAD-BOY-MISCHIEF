9430311e7a29392b4eabbe2bd0ee166f1c5b1fae
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-02-15 14:00:12 +0900

    title.scを追加

---
+++ BUDBOYMISCHIEF/Assets/katuki/Script/Title.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToStart()
+    {
+        Invoke("GoScene", 1);
+    }
+
+    public void Tutorial()
+    {
+        Invoke("GoTutorial", 0.5f);
+    }
+
+  private void GoScene()
+    {
+        //SceneManager.LoadScene("hogehoge");
+        Debug.Log("ToStart");
+    }
+
+   private void GoTutorial()
+    {
+        //SceneManager.LoadScene("hogehoge");
+        Debug.Log("ToTutorial");
+    }
+}
