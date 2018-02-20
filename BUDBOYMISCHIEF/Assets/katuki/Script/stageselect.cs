51cfaa816ddc750acb11ded578c7b227229f99d9
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-02-20 12:38:53 +0900

    stageselectにHighScoreTextを追加

--- BUDBOYMISCHIEF/Assets/katuki/Script/stageselect.cs
+++ BUDBOYMISCHIEF/Assets/katuki/Script/stageselect.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class stageselect : MonoBehaviour {  
-  
+public class stageselect : MonoBehaviour {
+    public AudioClip select;
+    private AudioSource audiosource;
+    public GameObject HighScore;
+    public Text[] stage;
+    private bool score = false;
     private int x;
+    private string ScoreString;
+    private int ScoreInt;
 	// Use this for initialization
 	void Start () {
-
+        audiosource = GetComponent<AudioSource>();
+        HighScore.SetActive(false);
         x = PlayerPrefs.GetInt("ClearStage");
         print(x);
+        for(int i = 1; i <= 5; i++)
+        {
+            ScoreInt = PlayerPrefs.GetInt("HighScore" + i);
+            ScoreString = ScoreInt.ToString();
+            stage[i-1].text = ScoreString;
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,7 +38,9 @@ public class stageselect : MonoBehaviour {
 	}
 
     public void Stage1()
-    {      
+    {
+        audiosource.clip = select;
+        audiosource.Play();
         SceneManager.LoadScene("Stage1");
         //Debug.Log("GO to Stage1");
     }
@@ -34,6 +49,8 @@ public class stageselect : MonoBehaviour {
     {
         if (x >= 1)
         {
+            audiosource.clip = select;
+            audiosource.Play();
             SceneManager.LoadScene("Stage2");
             //Debug.Log("GO to Stage2");
         }
@@ -43,6 +60,8 @@ public class stageselect : MonoBehaviour {
     {
         if (x >= 2)
         {
+            audiosource.clip = select;
+            audiosource.Play();
             SceneManager.LoadScene("Stage3");
             //Debug.Log("GO to Stage3");
         }
@@ -52,6 +71,8 @@ public class stageselect : MonoBehaviour {
     {
         if (x >= 3)
         {
+            audiosource.clip = select;
+            audiosource.Play();
             SceneManager.LoadScene("Stage4");
             //Debug.Log("GO to Stage4");
         }
@@ -61,8 +82,26 @@ public class stageselect : MonoBehaviour {
     {
         if (x >= 4)
         {
+            audiosource.clip = select;
+            audiosource.Play();
             SceneManager.LoadScene("Stage5");
             //Debug.Log("GO to Stage5");
+        }
+    }
+
+    public void Score()
+    {
+        audiosource.clip = select;
+        audiosource.Play();
+        if (score)
+        {
+            HighScore.SetActive(false);
+            score = false;
+        }
+        else
+        {
+            HighScore.SetActive(true);
+            score = true;
         }
     }
 }
